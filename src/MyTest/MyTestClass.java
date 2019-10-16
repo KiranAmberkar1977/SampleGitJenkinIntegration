8bdd80a7a8b75baf84dc3bab83450160dd4a5ce3
KiranAmberkar1977 <kiranamberkar@gmail.com>
2019-10-16 16:44:28 +0530

    Adding demo jenkins file

---
+++ src/MyTest/MyTestClass.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package MyTest;
+
+/**
+ * @author kiran
+ *
+ */
+public class MyTestClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+	}
+
+}
---
+++ src/demoJenkinsPackage/demoJenkins.java
@@ -0,0 +1,15 @@
+package demoJenkinsPackage;
+
+import org.testng.annotations.Test;
+
+public class demoJenkins {
+
+@Test
+public static void myTest ( ) {
+	short cnt;
+	for(cnt = 1; cnt < 9; cnt++) 
+		System.out.println("Hello world... Cnt is:" + cnt);
+	
+}
+	
+}
